7f6f2a9940c1d448b93093923a5a94a7d7879723
Jamie Sinn <jamie.sinn@taplytics.com>
2022-12-07 10:31:30 -0500

    More testing prints and messages.

--- test/testing.c
+++ test/testing.c
@@ -2,64 +2,8 @@
 // Created by Jamie Sinn on 2022-11-24.
 //
 
-#include <stdio.h>
 #include "testing.h"
 #include "../bucketing.h"
-#include <sys/time.h>
-#include <regex.h>
-#include <ctype.h>
-
-char *ltrim(char *s)
-{
-    while(isspace(*s)) s++;
-    return s;
-}
-
-char *rtrim(char *s)
-{
-    char* back = s + strlen(s);
-    while(isspace(*--back));
-    *(back+1) = '\0';
-    return s;
-}
-
-char *trim(char *s)
-{
-    return rtrim(ltrim(s));
-}
-
-char *getPayloadId(char *flushed_queue) {
-
-    char *payloadId;
-    regex_t payloadIdRegex;
-    regmatch_t rm[1];
-    int reti;
-    reti = regcomp(&payloadIdRegex, "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-(:?8|9|[aA]|[bB])[a-f0-9]{3}-[a-f0-9]{12}", REG_EXTENDED);
-    if (reti) {
-        fprintf(stderr, "Could not compile payloadIdRegex\n");
-        exit(1);
-    }
-
-    /* Execute regular expression */
-    reti = regexec(&payloadIdRegex, flushed_queue, 1, rm, 0);
-    if (!reti) {
-        puts("Match");
-        payloadId = malloc((int)(rm[0].rm_eo - rm[0].rm_so));
-        for (int i = 0; i < (int)(rm[0].rm_eo - rm[0].rm_so); i++)
-        {
-            payloadId[i] = flushed_queue[rm[0].rm_so+i];
-        }
-        printf("Payload Id: '%s'\n", payloadId);
-    } else if (reti == REG_NOMATCH) {
-        puts("No match");
-        exit(1);
-    } else {
-        fprintf(stderr, "Regex match failed\n");
-        exit(1);
-    }
-    regfree(&payloadIdRegex);
-    return payloadId;
-}
 
 int main() {
 
@@ -69,39 +13,58 @@ int main() {
     struct timeval stop, start;
 
 
-    printf("testing binary\n");
+    printf("DevCycle Local Bucketing Wrapper - Testing WASM Functionality\n");
+
+    printf("Initializing WASMTime engine... ");
     initialize();
+    printf("Success\n");
 
-
+    printf("Initializing Event Queue... ");
     init_event_queue(sdkKey, "{}");
+    printf("Success\n");
+    printf("Storing Environment Configuration... ");
     store_config(sdkKey, configString);
+    printf("Success\n");
+    printf("Storing Platform Data... ");
     set_platform_data(platformData);
-    unsigned char *bucketedConfig = generate_bucketed_config(sdkKey, "{\"user_id\":\"j_test\"}");
-    printf("%s\n", bucketedConfig);
-
-    // how fast is it to generate a bucketed config?
+    printf("Success\n");
+    printf("Generating a bucketed config... ");
     gettimeofday(&start, NULL);
-    bucketedConfig = generate_bucketed_config(sdkKey, "{\"user_id\":\"j_test\"}");
+    unsigned char *bucketedConfig = generate_bucketed_config(sdkKey, "{\"user_id\":\"j_test\"}");
     gettimeofday(&stop, NULL);
+    printf("Success\nBucketed User Config: %s\n", bucketedConfig);
     printf("took %lu microseconds to generate a bucketed config\n",
            (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
 
+    printf("Queuing multiple events... \n");
     queue_event(sdkKey, "{\"user_id\":\"j_test\"}", "{\"type\":\"testing\"}");
+    printf("1/3\n");
     queue_event(sdkKey, "{\"user_id\":\"j_test\"}", "{\"type\":\"testing2\"}");
+    printf("2/3\n");
     queue_event(sdkKey, "{\"user_id\":\"j_test\"}", "{\"type\":\"testing3\"}");
+    printf("3/3\n");
+    printf("Success\n");
+    printf("Getting Event Queue Size... ");
+    int equeue_size = event_queue_size(sdkKey);
+    assert(equeue_size == 3);
+    printf("Success\nEvent Queue Size: 3\n");
 
-    unsigned char *flushed_queue = flush_event_queue(sdkKey);
-    printf("%s\n", flushed_queue);
+    char *flushed_queue = flush_event_queue(sdkKey);
+    printf("Flushed Queue: '%s'\n", flushed_queue);
 
     char *payloadId = getPayloadId(flushed_queue);
 
+    printf("Marking queue as failed. Expect next flush to equal previous\n");
     on_payload_failure(sdkKey, payloadId, true);
-    assert(strcmp(flushed_queue, flush_event_queue(sdkKey)) == 0);
+    char *secondFlush = flush_event_queue(sdkKey);
+    assert(strcmp(flushed_queue, secondFlush)== 0);
+    printf("Flushed Queue: %s\n", flushed_queue);
 
+    printf("Marking queue as success. Expect next flush to equal empty array\n");
     on_payload_success(sdkKey, payloadId);
     flushed_queue = flush_event_queue(sdkKey);
     assert(strcmp("[]", flushed_queue) == 0);
-    printf("%s\n", flushed_queue);
+    printf("Flushed Queue: %s\n", flushed_queue);
 
     return 0;
 }
--- test/testing.h
+++ test/testing.h
@@ -4,5 +4,63 @@
 
 #ifndef DVCBUCKETING_TESTING_H
 #define DVCBUCKETING_TESTING_H
+#include <sys/time.h>
+#include <regex.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *ltrim(char *s)
+{
+    while(isspace(*s)) s++;
+    return s;
+}
+
+char *rtrim(char *s)
+{
+    char* back = s + strlen(s);
+    while(isspace(*--back));
+    *(back+1) = '\0';
+    return s;
+}
+
+char *trim(char *s)
+{
+    return rtrim(ltrim(s));
+}
+
+char *getPayloadId(char *flushed_queue) {
+
+    char *payloadId;
+    regex_t payloadIdRegex;
+    regmatch_t rm[1];
+    int reti;
+    reti = regcomp(&payloadIdRegex, "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-(:?8|9|[aA]|[bB])[a-f0-9]{3}-[a-f0-9]{12}", REG_EXTENDED);
+    if (reti) {
+        fprintf(stderr, "Could not compile payloadIdRegex\n");
+        exit(1);
+    }
+
+    /* Execute regular expression */
+    reti = regexec(&payloadIdRegex, flushed_queue, 1, rm, 0);
+    if (!reti) {
+        puts("Match");
+        payloadId = malloc((int)(rm[0].rm_eo - rm[0].rm_so));
+        for (int i = 0; i < (int)(rm[0].rm_eo - rm[0].rm_so); i++)
+        {
+            payloadId[i] = flushed_queue[rm[0].rm_so+i];
+        }
+        printf("Payload Id: '%s'\n", payloadId);
+    } else if (reti == REG_NOMATCH) {
+        puts("No match");
+        exit(1);
+    } else {
+        fprintf(stderr, "Regex match failed\n");
+        exit(1);
+    }
+    regfree(&payloadIdRegex);
+    return payloadId;
+}
 
 #endif //DVCBUCKETING_TESTING_H
